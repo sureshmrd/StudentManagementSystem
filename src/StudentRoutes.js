16ef3086321fc940c8d79abb2a5bd4c908e4e649
suresh <123853377+sureshmrd@users.noreply.github.com>
2024-06-17 18:03:38 +0530

    Delete src directory

--- src/StudentRoutes.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { RegNoForm } from './components/StudentLogin';
-import { Display } from './components/Display';
-
-// Nested routes for student
